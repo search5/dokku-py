928d76ed922f3ef0cf5d0d126dfee9f444ebe5a1
이지호 <search5@mz.co.kr>
2024-05-28 16:43:37 +0900

    CLI 0.1 draft

--- dokku_py/__init__.py
+++ dokku_py/__init__.py
@@ -3,8 +3,10 @@ import sys
 import shlex
 import os
 import re
-from subprocess import run, DEVNULL, PIPE
+from functools import partial
+from subprocess import run, DEVNULL, PIPE, CalledProcessError
 
+print_error = partial(print, file=sys.stderr)
 dokku_env = {}
 
 
@@ -14,33 +16,52 @@ def fn_get_remote():
     return result.stdout.decode('utf-8') or 'dokku'
 
 
-def git_rev_parse_result(dokku_git_remote):
-    git_rev_parse = run(["git", "rev-parse", "--git-dir"], stdout=DEVNULL, stderr=DEVNULL)
-    if os.path.exists(".git") or git_rev_parse.returncode == 0:
-        pattern = re.compile(
-            rf"^{re.escape(dokku_git_remote)}\s",
-            re.IGNORECASE|re.MULTILINE)
-        git_remote_cmd = run(["git", "remote", "-v"],
-                             stderr=DEVNULL,
-                             stdout=PIPE)
-        cmd_stdout = git_remote_cmd.stdout.decode('utf-8')
+def is_git_repository():
+    try:
+        run(["git", "rev-parse", "--git-dir"],
+            stdout=DEVNULL, stderr=PIPE, check=True)
+        return True
+    except CalledProcessError:
+        return False
 
-        return (cmd_stdout if pattern.search(cmd_stdout) else '',
-                git_remote_cmd.returncode)
-    else:
-        return '', ''
+
+def get_dokku_app_name(dokku_git_remote, dokku_remote_host):
+    try:
+        result = run(
+            ["git", "remote", "-v"],
+            stdout=PIPE,
+            stderr=DEVNULL,
+            check=True,
+            text=True
+        )
+        remotes = result.stdout.splitlines()
+        for remote in remotes:
+            if (remote.startswith(dokku_git_remote)
+                    and f"dokku@{dokku_remote_host}" in remote):
+                app_name = remote.split('@')[1].split(':')[1].split()[0]
+                return app_name
+    except CalledProcessError:
+        return None
+    return None
 
 
 def fn_dokku_host(dokku_git_remote, dokku_host):
     if not dokku_host:
-        cmd_stdout, ret_code = git_rev_parse_result(dokku_git_remote)
-        if cmd_stdout:
-            git_dokku_rev = cmd_stdout.splitlines()[0]
-            git_dokku_host = git_dokku_rev.split(" ")[0].split("@")[1]
-            dokku_host = git_dokku_host.split(":")[0] if ret_code == 0 else ''
-
-    if not dokku_host:
-        return ''
+        if os.path.isdir(".git") or is_git_repository():
+            try:
+                result = run(
+                    ["git", "remote", "-v"],
+                    stdout=DEVNULL,
+                    check=True,
+                    text=True
+                )
+                remotes = result.stdout.splitlines()
+                for remote in remotes:
+                    if remote.startswith(dokku_git_remote):
+                        dokku_host = remote.split()[1].split('@')[1].split(':')[0]
+                        break
+            except CalledProcessError:
+                pass
 
     return dokku_host
 
@@ -62,6 +83,44 @@ def fn_random_name():
     return f"{WORD1}-{WORD2}-{WORD3}".lower()
 
 
+def ssh_command(command):
+    """Execute an SSH command and return the output."""
+    try:
+        result = run(shlex.split(command), check=True, stdout=PIPE, stderr=PIPE)
+        return result.stdout.decode('utf-8')
+    except CalledProcessError as e:
+        return e.stderr.decode('utf-8')
+
+
+def fn_is_git_repo():
+    try:
+        run(
+            ["git", "rev-parse"],
+            stdout=DEVNULL,
+            check=True
+        )
+        return True
+    except CalledProcessError:
+        return False
+
+
+def fn_has_dokku_remote(dokku_git_remote):
+    try:
+        result = run(
+            ["git", "remote", "show"],
+            stdout=PIPE,
+            check=True,
+            text=True
+        )
+        remotes = result.stdout.splitlines()
+        for remote in remotes:
+            if remote.startswith(dokku_git_remote):
+                return True
+        return False
+    except CalledProcessError:
+        return False
+
+
 def main():
     dokku_env['DOKKU_TRACE'] = os.getenv('DOKKU_TRACE', 0)
     dokku_env['DOKKU_PORT'] = os.getenv('DOKKU_PORT', 22)
@@ -69,7 +128,6 @@ def main():
 
     app = ''
     dokku_git_remote = fn_get_remote()
-    dokku_remote_host = ''
 
     sys_argv = sys.argv[1:]
 
@@ -86,10 +144,8 @@ def main():
             next_index += 1
             skip = False
             continue
-        is_flag = False
 
-        if arg.startswith('--'):
-            is_flag = True
+        is_flag = arg.startswith('--')
 
         if arg == "--app":
             app = args[next_index]
@@ -100,13 +156,13 @@ def main():
             skip = True
             args = args[2:]
         elif arg.startswith("--"):
-            if cmd_set and (not is_flag):
+            if cmd_set and not is_flag:
                 app_arg = arg
                 break
             if arg == "--trace":
                 dokku_env['DOKKU_TRACE'] = 1
         else:
-            if cmd_set and (not is_flag):
+            if cmd_set and not is_flag:
                 app_arg = arg
             else:
                 cmd = arg
@@ -120,130 +176,94 @@ def main():
         fn_client_help_msg()
 
     if not app:
-        escape_host = f"dokku@{dokku_remote_host}"
-        pattern = re.compile(
-            rf"{escape_host}",
-            re.IGNORECASE | re.MULTILINE)
-        cmd_stdout, ret_code = git_rev_parse_result(dokku_git_remote)
-        if ret_code == 0:
-            if cmd_stdout and pattern.search(cmd_stdout):
-                git_dokku_rev = cmd_stdout.splitlines()[0]
-                app = git_dokku_rev.split(" ")[0].split("@")[1].split(":")[1]
+        if os.path.isdir(".git") or is_git_repository():
+            app = get_dokku_app_name(dokku_git_remote, dokku_remote_host)
         else:
-            print(" !     This is not a git repository")
+            print_error("This is not a git repository")
 
     match cmd:
         case "apps:create":
             if not app and not app_arg:
                 app = fn_random_name()
                 counter = 0
-                ssh_command = f"ssh -p {dokku_env['DOKKU_PORT']} dokku@{dokku_remote_host} apps"
-                # while ssh -p "$DOKKU_PORT" "dokku@$DOKKU_REMOTE_HOST" apps 2>/dev/null | grep -q "$APP"; do
                 while True:
+                    command = f"ssh -p {dokku_env['DOKKU_PORT']} dokku@{dokku_remote_host} apps"
+                    apps_output = ssh_command(command)
+                    if app not in apps_output:
+                        break
                     if counter >= 100:
-                        print(" !     Could not reasonably generate a new app name. Try cleaning up some apps...")
-                        # ssh -p "$DOKKU_PORT" "dokku@$DOKKU_REMOTE_HOST" apps
-                        sys.exit(1)
+                        print_error(" !     Could not reasonably generate a new app name. Try cleaning up some apps...")
+                        print(apps_output)
+                        exit(1)
                     else:
+                        # TODO fixed callable
                         app = 'random_name'
                         counter += 1
             elif not app:
                 app = app_arg
-            """
-            # if git remote add "$DOKKU_GIT_REMOTE" "dokku@$DOKKU_REMOTE_HOST:$APP"; then
-            #   echo "-----> Dokku remote added at ${DOKKU_REMOTE_HOST} called ${DOKKU_GIT_REMOTE}"
-            #   echo "-----> Application name is ${APP}"
-            # else
-            #   echo " !     Dokku remote not added! Do you already have a dokku remote?" 1>&2
-            #   return
-            # fi
-            # ;;
-        """
+            command = f'git remote add {dokku_git_remote} dokku@{dokku_remote_host}:{app}'
+            try:
+                run(shlex.split(command), check=True)
+                print(f"-----> Dokku remote added at {dokku_remote_host} called {dokku_git_remote}")
+                print(f"-----> Application name is {app}")
+            except CalledProcessError:
+                print_error(" !     Dokku remote not added! Do you already have a dokku remote?")
         case "apps:destroy":
-            pass
+            if fn_is_git_repo() and fn_has_dokku_remote(dokku_git_remote):
+                run(["git", "remote", "remove", dokku_git_remote],
+                    check=True)
         case "remote":
-            pass
+            print(dokku_git_remote)
+            exit(0)
         case "remote:add":
-            pass
+            args.pop(0)
+            exit(run(["git", "remote", "add"] + args).returncode)
         case "remote:list":
-            pass
+            exit(run(["git", "remote"]).returncode)
         case "remote:set":
-            pass
+            args.pop(0)
+            exit(run(["git", "config", "dokku.remote"] + args).returncode)
         case "remote:remove":
-            pass
+            args.pop(0)
+            exit_code = run(["git", "remote", "remove"] + args).returncode
+            if run(["git", "config", "dokku.remote"],
+                    stdout=PIPE, text=True).stdout.strip() == args[0]:
+                run(["git", "config", "--unset", "dokku.remote"])
+            exit(exit_code)
         case "remote:unset":
-            pass
+            if run(["git", "config", "dokku.remote"],
+                   stdout=PIPE, text=True).stdout.strip():
+                exit(run(["git", "config", "--unset", "dokku.remote"]).returncode)
+            exit(0)
 
-    #   case "$CMD" in
-    #     apps:create)
+    print(app)
+    commands = ["apps", "certs", "help", "ls", "nginx", "shell", "storage",
+                "trace", "version"]
+    if any(command in cmd for command in commands):
+        app = None
 
-    #     apps:destroy)
-    #       fn-is-git-repo && fn-has-dokku-remote && git remote remove "$DOKKU_GIT_REMOTE"
-    #       ;;
-    #     remote)
-    #       echo "$DOKKU_GIT_REMOTE"
-    #       exit 0
-    #       ;;
-    #     remote:add)
-    #       shift 1
-    #       git remote add "$@"
-    #       exit "$?"
-    #       ;;
-    #     remote:list)
-    #       git remote
-    #       exit "$?"
-    #       ;;
-    #     remote:set)
-    #       shift 1
-    #       git config dokku.remote "$@"
-    #       exit "$?"
-    #       ;;
-    #     remote:remove)
-    #       shift 1
-    #       git remote remove "$@"
-    #       local exit_code="$?"
-    #       if [[ "$(git config dokku.remote)" == "$1" ]]; then
-    #         git config --unset dokku.remote
-    #       fi
-    #       exit "$exit_code"
-    #       ;;
-    #     remote:unset)
-    #       if [[ -n "$(git config dokku.remote)" ]]; then
-    #         git config --unset dokku.remote
-    #         exit "$?"
-    #       fi
-    #       exit 0
-    #       ;;
-    #   esac
-    #
-    #   [[ " apps certs help ls nginx shell storage trace version " == *" $CMD "* ]] && unset APP
-    #   [[ " certs:chain domains:add-global domains:remove-global domains:set-global ps:restore " == *" $CMD "* ]] && unset APP
-    #   [[ " storage:ensure-directory " == *" $CMD "* ]] && unset APP
-    #   [[ "$CMD" =~ events*|plugin*|ssh-keys* ]] && unset APP
-    #   [[ -n "$APP_ARG" ]] && [[ "$APP_ARG" == "--global" ]] && unset APP
-    #   if [[ -n "$@" ]] && [[ -n "$APP" ]]; then
-    #     set -- "$APP" "$@"
-    #     set -- "--app" "$@"
-    #   fi
-    #   # echo "ssh -o LogLevel=QUIET -p $DOKKU_PORT -t dokku@$DOKKU_REMOTE_HOST -- $app_arg $@"
-    #   local ssh_args=("-o" "LogLevel=QUIET" "-p" "$DOKKU_PORT" "-t" "dokku@$DOKKU_REMOTE_HOST" "--")
-    #   ssh_args+=("$@")
-    #   ssh "${ssh_args[@]}" || {
-    #     ssh_exit_code="$?"
-    #     echo " !     Failed to execute dokku command over ssh: exit code $?" 1>&2
-    #     echo " !     If there was no output from Dokku, ensure your configured SSH Key can connect to the remote server" 1>&2
-    #     return $ssh_exit_code
-    #   }
-#
-# fn-is-git-repo() {
-#   git rev-parse &>/dev/null
-# }
-#
-# fn-has-dokku-remote() {
-#   git remote show | grep -E "^${DOKKU_GIT_REMOTE}\s"
-# }
-#
-# if [[ "$0" == "dokku" ]] || [[ "$0" == *dokku_client.sh ]] || [[ "$0" == $(command -v dokku) ]]; then
-#   main "$@"
-#   exit $?
-# fi
+    commands = ["certs:chain", "domains:add-global", "domains:remove-global",
+                "domains:set-global", "ps:restore"]
+    if any(command in cmd for command in commands):
+        app = None
+
+    if "storage:ensure-directory" in cmd:
+        app = None
+
+    if cmd.startswith("events") or cmd.startswith("plugin") or cmd.startswith("ssh-keys"):
+        app = None
+
+    if app_arg and app_arg == "--global":
+        app = None
+
+    if len(args) and app:
+        args[0:0] = ("--app", app)
+
+    # print(f"ssh -o LogLevel=QUIET -p {dokku_env['DOKKU_PORT']} -t dokku@{dokku_remote_host} -- {app_arg} {' '.join(args)}")
+    ssh_args = ["-o", "LogLevel=QUIET", "-p", dokku_env['DOKKU_PORT'], "-t", f"dokku@{dokku_remote_host}", "--"]
+    ssh_args.extend(args)
+    result = run(["ssh"] + ssh_args, text=True)
+    if result.returncode != 0:
+        print_error(f"Failed to execute dokku command over ssh: exit code {result.returncode}")
+        print_error("If there was no output from Dokku, ensure your configured SSH Key can connect to the remote server")
+        exit(result.returncode)
